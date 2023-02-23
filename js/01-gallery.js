463993a8e2e14dd841b8bf5cb3cd43af26ad441e
Ddemkowicz <dakucp@gmail.com>
2023-02-23 09:57:06 +0100

    Zadanie 1 DONE

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,35 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+for (let i = 0; i < galleryItems.length; i++) {
+  const image = document.createElement("img");
+  image.classList = "gallery__image";
+  image.src = galleryItems[i].preview;
+  image.alt = galleryItems[i].description;
+  image.setAttribute("data-source", `${galleryItems[i].original}`);
+  const link = document.createElement("a");
+  link.classList = "gallery__link";
+  link.href = galleryItems[i].original;
+  const div = document.createElement("div");
+  div.classList = "gallery__item";
+  link.append(image);
+  div.append(link);
+  gallery.append(div);
+
+  div.addEventListener("click", (e) => {
+    e.preventDefault();
+    const instance = basicLightbox.create(
+      `<img src=${galleryItems[i].original} width="800" height="600">`
+    );
+
+    instance.show();
+
+    document.addEventListener("keydown", (e) => {
+      if (e.code === "Escape") {
+        instance.close();
+      }
+    });
+  });
+}
